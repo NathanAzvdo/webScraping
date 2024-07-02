696ce346764770929cbf3f2b853df0c9d2bd5893
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-07-02 19:15:12 -0300

    gerando dataFrame com Pandas, csv e xlsx com as noticias

--- scrapingG1.py
+++ scrapingG1.py
@@ -1,19 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-
+lista_noticias = []
 
 response = requests.get('https://g1.globo.com/')
 content = response.content
 
 site =BeautifulSoup(content)
 
-notice = site.find('div', attrs={'class': 'feed-post-body'})
-title = notice.find('a', attrs={'class': 'feed-post-link'})
-resumo = notice.find('div', attrs={'class': 'feed-post-body-resumo'})
+notices = site.findAll('div', attrs={'class': 'feed-post-body'})
 
 
+for notice in notices:
+    title = notice.find('a', attrs={'class': 'feed-post-link'})
+    resumo = notice.find('div', attrs={'class': 'feed-post-body-resumo'})
+    if resumo:
+        lista_noticias.append([title.text, resumo.text, title["href"]])
+    else:
+        lista_noticias.append([title.text,' ',title["href"]])
+        
 
+news = pd.DataFrame(lista_noticias, columns=['Título', "Resumo", "Link"])
+news.to_csv('noticias.csv', index=False)#index false para não salvar o índice do DataFrame
+news.to_excel('noticias.xlsx', index=False)
 
-print(title.text)
-print(resumo.text)
+print(news)
+
+    
+
